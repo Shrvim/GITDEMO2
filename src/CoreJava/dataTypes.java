4e38cb46fd5c9d071f719d166ef904cedf236d82
Shrvim <shrutika1351@gmail.com>
2023-02-28 01:43:42 -0500

    Commit 2

--- src/CoreJava/dataTypes.java
+++ src/CoreJava/dataTypes.java
@@ -13,6 +13,9 @@ public class dataTypes {
 		System.out.println(num );
 		System.out.println("Hello");
 		System.out.println("Hello2");
+		System.out.println("Hello3");
+		System.out.println("Hello4");
+		System.out.println("Hello5");
 		
 		
 		
